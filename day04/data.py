6c297cc0c48c0598f243ffbc68052833db8c477b
caixin_d <43961402+18483666678@users.noreply.github.com>
2020-05-04 14:04:43 +0800

    Update data.py

--- day04/data.py
+++ day04/data.py
@@ -4,12 +4,13 @@ from torch import nn
 import os
 from PIL import Image
 from torchvision import transforms
-
+import numpy as np
 
 tf = transforms.ToTensor()
 
+
 class MyDataset(Dataset):
-    def __init__(self,root):
+    def __init__(self, root):
         self.root = root
         self.dataset = os.listdir(root)
 
@@ -18,11 +19,13 @@ class MyDataset(Dataset):
 
     def __getitem__(self, index):
         fn = self.dataset[index]
-        strs = fn.split('.')
-        labels = [int(x) for x in strs]
+        strs = fn.split('.')[0]
+        labels = np.array([int(x) for x in strs])
         img_data = tf(Image.open(f"{self.root}/{fn}"))
-        return img_data,labels
+        return img_data, labels
+
 
 if __name__ == '__main__':
     myDataset = MyDataset("./code")
     print(myDataset[0])
+    print(myDataset[0][0].shape) # [3,60,240]
