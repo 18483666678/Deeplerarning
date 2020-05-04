be804b4ebc3a4e69a30568c7b40e403660bb8d99
caixin_d <43961402+18483666678@users.noreply.github.com>
2020-05-04 14:05:44 +0800

    Update train.py

--- day04/train.py
+++ day04/train.py
@@ -1,14 +1,14 @@
 import torch
-from day04.net import *
-from  torch.utils.data import DataLoader
-from day04.data import *
+from net import *
+from torch.utils.data import DataLoader
+from data import *
 from torch import optim
 
 
 class Trainer:
     def __init__(self):
         train_dataset = MyDataset("./code")
-        self.train_dataloader = DataLoader(train_dataset,100,True)
+        self.train_dataloader = DataLoader(train_dataset, 100, True)
 
         self.net = Cnn2SEQ()
 
@@ -16,20 +16,30 @@ class Trainer:
 
         self.loss_fn = nn.CrossEntropyLoss()
 
-    def __call__(self,):
-        for epoch in range(10000):
-            for i,(img,tag) in enumerate(self.train_dataloader):
-                output = self.net(img)
-                output = output.reshape(-1,10)
-                tag = tag.reshpe(-1)
-                loss = self.loss_fn(output,tag.long())
+    def __call__(self,EPOCHS):
+        for epoch in range(EPOCHS):
+            for i, (img, tag) in enumerate(self.train_dataloader):
+                output = self.net(img)  # [100,4,10]
+                output = output.reshape(-1, 10)  # [400,10]
+
+                tag = tag.reshape(-1)  # [100,4]-->[400]
+
+                loss = self.loss_fn(output, tag.long())
 
                 self.opt.zero_grad()
                 loss.backward()
                 self.opt.step()
 
-                print(loss)
+                output = torch.argmax(output, dim=1)
+                bool = tag.eq(output.data).cpu().sum()
+                accuracy = int(bool) / tag.size(0)
+
+                print(f"epoch:{epoch}/{EPOCHS} | loss:{loss} | accuracy:{accuracy}")
+                print('output:', output[:4].item())
+                print("label:", tag[:4].item())
+
+
 
 if __name__ == '__main__':
     train = Trainer()
-    train()
+    train(10000)
